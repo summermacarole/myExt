d588d3ce5ee14afcd4bd66526b963b9ac27e8801
littlemacarole <1740249919@qq.com>
2017-02-20 21:47:28 +0800

    Initial commit

---
+++ demo1/demo1.js
@@ -0,0 +1,29 @@
+(function () {
+    Ext.onReady(function () {
+        // alert('hello')
+        // Ext.Msg.alert('我日你大爷呀')
+        // Ext.define('Base',{
+        //     constructor:function(config){
+        //         console.log('我执行了吗')
+        //     },
+        //     statics:{
+        //         showInfo:function(){
+        //             console.log('我可以显示信息，哈哈');
+        //         }
+        //     },
+        //     getName:function(){
+        //         console.log('我可以获取别人的姓名，哈哈')
+        //     }
+        // }),
+        //     Ext.define('Son',{
+        //         extend:'Base',
+        //         constructor:function(){
+        //             this.callParent();
+        //
+        //         }
+        //     })
+        //
+        // var son=new Son();
+        // son.getName()
+    })
+})();
